393937e9fcd30153605abe53a808944d8b524acf
Doug Walter <doug@kosmojo.com>
2014-11-23 19:09:42 -0800

    implent authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   def index
   	@posts = Post.all
-    # puts "VALUE of @posts VARIABLE IN INDEX CONTROLLER: #{@posts.inspect}"
   end
 
   def show
@@ -13,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = current_user.posts.build(params.require(:post).permit(:title, :body))
     if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
