4b8bfd4296008fe905a8f0a9993bd63c65828f8a
Doug Walter <doug@kosmojo.com>
2014-11-24 00:52:33 -0800

    create and implement comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :posts
+ has_many :posts, dependent: :destroy
+ has_many :comments, dependent: :destroy
+
 
  mount_uploader :avatar, AvatarUploader
 
