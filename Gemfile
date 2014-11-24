d9a6c753f14fd5f858b24218518b9af4645254b8
Doug Walter <doug@kosmojo.com>
2014-11-23 23:47:05 -0800

    added avatar uploader to user

--- Gemfile
+++ Gemfile
@@ -45,3 +45,9 @@ gem 'figaro', '1.0'
 gem 'pundit'
 
 gem 'redcarpet'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
+
+gem 'fog'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
+ mount_uploader :avatar, AvatarUploader
+
   def admin?
   	role == 'admin'
   end
